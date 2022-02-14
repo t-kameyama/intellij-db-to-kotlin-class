92b98752646906bb3234eea25d9ede4b6b49352c
GitHub Action <action@github.com>
2022-02-14 00:09:35 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/tkameyama/intellijdbtokotlinclass/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.tkameyama.intellijdbtokotlinclass.services
+
+import com.github.tkameyama.intellijdbtokotlinclass.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
--- src/main/kotlin/com/github/tkameyama/intellijdbtokotlinclass/services/MyProjectService.kt
+++ src/main/kotlin/com/github/tkameyama/intellijdbtokotlinclass/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.tkameyama.intellijdbtokotlinclass.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.tkameyama.intellijdbtokotlinclass.MyBundle
 
 class MyProjectService(project: Project) {
 
